10c3bdbe2cd6d7598b720056586ecbc052f26a58
inan yigit <inanygt@hotmail.com>
2024-02-04 20:47:11 +0100

    fetch games from api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,45 @@
-<script setup>
+<script>
+import { onMounted, ref } from 'vue';
 
+export default {
+  
+  setup() {
+    const posts = ref([]);
+    const error = ref(null);
+
+    const load = async () => {
+
+      try {
+      let data = await fetch('http://localhost:8000/api/games')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+
+        posts.value = await data.json()
+        console.log(posts.value)
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+
+  }
+    
+  
+  load();
+
+    return { posts }
+  }
+}
+
+
+
+
+  
 </script>
 
 <template>
-  <main>
-  </main>
+  <div v-for="post in posts">
+    <div>{{ post.name }}</div>
+  </div>
 </template>
